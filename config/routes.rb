5f5b7f75c29af057530fc547ece44bd2a298742e
G Palmer <palmergs@gmail.com>
2024-06-20 07:37:30 -0400

    adding a placeholder home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :passages
   resources :props
   resources :actors
@@ -12,5 +13,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 end
