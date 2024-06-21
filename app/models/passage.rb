51dcdaf613182f4ed79accf785c63fd451d562c5
G Palmer <palmergs@gmail.com>
2024-06-21 12:48:42 -0400

    docker compose working with tailwindcss:watch but file changes do not appear to be picked up

--- app/models/passage.rb
+++ app/models/passage.rb
@@ -1,4 +1,6 @@
 class Passage < ApplicationRecord
   belongs_to :from, class_name: 'Stage'
   belongs_to :to, class_name: 'Stage'
+
+  validates :direction, presence: true, uniqueness: { scope: :to_id }
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,11 +2,15 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  puts "Setting development values..."
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
+
+  # Try a different file watcher for Docker container
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 
   # Do not eager load code on boot.
   config.eager_load = false
